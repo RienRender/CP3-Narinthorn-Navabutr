1a35673902224ce9b93162ad40da04374d5fd72c
RienRender <67402796+RienRender@users.noreply.github.com>
2024-06-18 13:36:55 +0700

    Update Exercise4_Narinthorn_N.py

--- Exercise4_Narinthorn_N.py
+++ Exercise4_Narinthorn_N.py
@@ -1,1 +1,11 @@
+FoundEng = 10.0
+GenBui = 9.3
+IntroComSys = 9.7
+ComPro = 10.0
+
+print("--- Your Score ---")
+print("Foundation English :", FoundEng)
+print("General Business :", GenBui)
+print("Introduction to Computer Systems :", IntroComSys)
+print("Computer Programming :", ComPro)
 
